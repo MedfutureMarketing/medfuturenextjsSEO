34c9392c1596b589c8fdd862d265448f964c3d6f
A Nilshan <medfuturedigitalmarketing@gmail.com>
2025-11-21 07:36:44 +0530

    Mobile update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-module.exports = {
-  experimental: {
-    optimizeCss: true
-  }
+   experimental: {
+    optimizeCss: true, // inlines critical CSS to remove render-blocking
+  },
 };
 
 
