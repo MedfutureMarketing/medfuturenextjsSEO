c668c3033482c11af717253174d642570a249610
A Nilshan <medfuturedigitalmarketing@gmail.com>
2025-11-26 11:24:31 +0530

    Schema Markup Update

--- src/components/JobBoard/Searchbar.tsx
+++ src/components/JobBoard/Searchbar.tsx
@@ -86,7 +86,7 @@ export default function SearchBarWithLocation() {
               value={query}
               onChange={(e) => setQuery(e.target.value)}
             />
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -99,7 +99,7 @@ export default function SearchBarWithLocation() {
                 strokeLinejoin="round"
                 d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
               />
-            </svg>
+            </svg> */}
           </div>
 
           {/* LOCATION WRAPPER */}
