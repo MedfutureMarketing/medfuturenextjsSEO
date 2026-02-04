2edaccca3ee34cc2034eb4b4b7b8f1679a6aae31
Medfuture <medfuturedigitalmarketing@gmail.com>
2026-02-04 10:03:47 +0530

    ss

--- src/app/ahp-division/page.tsx
+++ src/app/ahp-division/page.tsx
@@ -1,11 +1,13 @@
 
 import Ahpdivsionhero from "@/components/AhpDivision/AhpdivisionComponents/DivisionHero"
 import AhpDivisionContentsection from "@/components/AhpDivision/AhpdivisionComponents/AhpDivisionContentsection"
+import Professionsection from "@/components/AhpDivision/AhpdivisionComponents/ProfessionSection"
 const AhpDivisionPage = () => {
     return (
         <div>
             <Ahpdivsionhero />
             <AhpDivisionContentsection />
+            <Professionsection />
         </div>
     );
 };
