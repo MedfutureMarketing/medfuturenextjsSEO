d6778b4f19b20302856aaa42f959359ea60a1c6c
Medfuture <medfuturedigitalmarketing@gmail.com>
2026-02-06 14:22:14 +0530

    SS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en-AU" className={inter.className}>
       <body className="antialiased">
 
         {/* Preloader */}
