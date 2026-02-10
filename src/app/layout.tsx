884dd92770c862300c2e919a706f71f6d428bf6f
Medfuture <medfuturedigitalmarketing@gmail.com>
2026-02-10 14:07:37 +0530

    ss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en-AU" className={inter.className}>
+      <head><script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vf3wuaoe41");
+</script></head>
       <body className="antialiased">
 
         {/* Preloader */}
