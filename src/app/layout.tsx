e4ac353f56eb782556de1cb36e8d70b18f018a00
Medfuture <medfuturedigitalmarketing@gmail.com>
2026-02-10 14:09:39 +0530

    ss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import Breadcrumb from "@/components/Breadcrumb";
 import Preloader from "@/components/Preloader";
 import Footer from "@/components/Footer";
 import BottomNav from "@/components/Navbar/BottomNavbar";
+import Script from "next/script";
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,13 +28,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en-AU" className={inter.className}>
-      <head><script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "vf3wuaoe41");
-</script></head>
+      <head> <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vf3wuaoe41");
+            `,
+          }}
+        /></head>
       <body className="antialiased">
 
         {/* Preloader */}
