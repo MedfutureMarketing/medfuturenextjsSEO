20beb935f13ff3d25bd29c38f50bdceea9ac1b01
Medfuture <medfuturedigitalmarketing@gmail.com>
2026-03-05 08:30:22 +0530

    ss

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,10 +1,22 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import SignupComponent from '@/components/Signup';
 
 export default function Signup() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('TOKEN');
+    if (token) {
+      router.replace('/my-account/candidate/profile');
+    }
+  }, [router]);
+
   return (
-    <main >
-     <SignupComponent />
+    <main>
+      <SignupComponent />
     </main>
   );
 }
-
